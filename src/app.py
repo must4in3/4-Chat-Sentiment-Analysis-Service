ed17b8193bdc84062ba174144f06144f1046aba7
must4in3 <nicola.liscia@tiscali.it>
2020-07-13 01:17:11 +0200

    clean app.py file

--- src/app.py
+++ src/app.py
@@ -1,21 +1,3 @@
 from flask import Flask, request
 
 app=Flask(__name__)
-
-@app.route('/a')
-def saluda():
-    return 'hola'
-
-
-### L1. User endpoints
-# - (GET) `/user/create/<username>`
-
-
-
-
-
-### L1. Chat endpoints
-#- (GET) `/chat/create`
-# (GET) `/chat/<conversation_id>/adduser`
-# - (POST) `/chat/<conversation_id>/addmessage`
-#- (GET) `/chat/<conversation_id>/list`
