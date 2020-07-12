9d5223d8a362895055f6ab6b4e54dd2a172ddb4c
must4in3 <nicola.liscia@tiscali.it>
2020-07-12 18:53:20 +0200

    update gitignore

---
+++ src/controllers/sentiment_analysis_endpoint.py
@@ -0,0 +1,50 @@
+from src.app import app
+from flask import request
+from pymongo import MongoClient
+import json
+from bson import json_util, ObjectId
+from src.config import DBURL
+from src.helpers.errorHelpers import errorHelper, APIError, Error404, checkValidParams
+#nltk libraries and modules
+from nltk.tokenize import sent_tokenize,word_tokenize
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('vader_lexicon')
+nltk.download('averaged_perceptron_tagger')
+
+client = MongoClient(DBURL)
+print(f'connected to db {DBURL}')
+db = client.get_default_database()
+
+
+@app.route("/chat/<conversation_id>/sentiment")
+@errorHelper()
+def sentimentAnalysis(conversation_id):
+    print(f'Requesting to a sentiment analysis from the chat-room {conversation_id}')
+    chat = db.chatItem.find_one({'_id': ObjectId(conversation_id)})
+    if chat:
+        chat_json = json.loads(json_util.dumps(chat))
+        chat_mess = []
+        for message in chat_json.items():
+            for mess in message[1]:
+                try:
+                    chat_mess.append(mess['message'])
+                except:
+                    pass
+        pol = polarityBySentence(chat_mess)
+        return json.dumps(pol)
+    raise APIError('Error. chat doesn\'t exist')   
+
+
+def polarityBySentence(phrases_list):
+    sid = SentimentIntensityAnalyzer()
+    pol = []
+    for phrase in phrases_list:
+        tokenized_text=sent_tokenize(phrase)
+        print(tokenized_text)
+        for sentence in tokenized_text:
+            scores = sid.polarity_scores(sentence)
+            pol.append({'sentence': sentence, 'sentiment': scores})
+    return pol
